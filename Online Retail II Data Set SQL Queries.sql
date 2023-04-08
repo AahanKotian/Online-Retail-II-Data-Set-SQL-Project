0c6a1bf196706a79f6fac567e198c3fd2b22ae4d
Akotian79 <106942580+Akotian79@users.noreply.github.com>
2023-04-07 23:28:18 -0400

    Update Online Retail II Data Set SQL Queries.sql

--- Online Retail II Data Set SQL Queries.sql
+++ Online Retail II Data Set SQL Queries.sql
@@ -1,13 +1,13 @@
 
-"Calculate the monthly revenue for the year 2011"
+-- Calculate the monthly revenue for the year 2011"
 SELECT SUM(CAST(Quantity AS INT)) AS TotalQuantity
 FROM online_retail_II
 WHERE ISNUMERIC(Quantity) = 1;
 
-"Calculate total number of orders in the dataset"
+-- Calculate total number of orders in the dataset"
 SELECT COUNT(*) AS TotalOrders FROM [dbo].[online_retail_II]
 =1050924
 
-"Identify the amount of distinct StockCodes"
+--Identify the amount of distinct StockCodes"
 SELECT COUNT(DISTINCT StockCode) AS TotalOrders
 FROM online_retail_II 
